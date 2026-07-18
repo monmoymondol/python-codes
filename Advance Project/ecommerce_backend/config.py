82942851369cb459a9fe980af9e81a7bf1ccb92d
Sagor Mondol <135149222+monmoymondol@users.noreply.github.com>
2026-07-19 02:18:00 +0600

    Add configuration class for database and secrets

---
+++ Advance Project/ecommerce_backend/config.py
@@ -0,0 +1,5 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = "sqlite:///ecommerce.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "supersecretkey"
+    JWT_SECRET_KEY = "jwtsecretkey"
